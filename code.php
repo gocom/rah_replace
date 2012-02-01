3ee7595b6ae798d4b53f72d388c1b8fc6424692b
Jukka Svahn <web@rahinaa.biz>
2012-02-01 02:20:32 +0200

    Adding v0.3 from SVN.

--- code.php
+++ code.php
@@ -1,23 +1,26 @@
 <?php	##################
 	#
 	#	rah_replace-plugin for Textpattern
-	#	version 0.2
+	#	version 0.3
 	#	by Jukka Svahn
 	#	http://rahforum.biz
 	#
 	###################
 
 	function rah_replace($atts,$thing) {
-		global $pretext;
+		
 		extract(lAtts(array(
 			'from' => '',
 			'to' => '',
 			'delimiter' => ','
 		),$atts));
-		$from = explode($delimiter,$from);
-		$to = explode($delimiter,$to);
-		$count = count($to);
-		if($count == 1) $to = implode('',$to);
-		if($count == 0) $to = '';
+		
+		if($delimiter !== '') {
+			$from = explode($delimiter,$from);
+			$to = explode($delimiter,$to);
+			$to = count($to) <= 1 ? implode('',$to) : $to;
+		}
+		
 		return str_replace($from,$to,parse($thing));
-	}?>
+	}
+?>
