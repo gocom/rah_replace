f5c9baf9d9551319d12bf701d2e7de672edc3db3
Jukka Svahn <web@rahinaa.biz>
2013-01-17 12:02:57 +0200

    Updated comments.

--- rah_replace.php
+++ rah_replace.php
@@ -1,14 +1,14 @@
 <?php
 
 /**
- * Rah_replace plugin for Textpattern CMS
+ * Rah_replace plugin for Textpattern CMS.
  *
  * @author Jukka Svahn
- * @date 2009-
+ * @date    2009-
  * @license GNU GPLv2
- * @link http://rahforum.biz/plugins/rah_replace
+ * @link    http://rahforum.biz/plugins/rah_replace
  *
- * Copyright (C) 2012 Jukka Svahn <http://rahforum.biz>
+ * Copyright (C) 2012 Jukka Svahn http://rahforum.biz
  * Licensed under GNU Genral Public License version 2
  * http://www.gnu.org/licenses/gpl-2.0.html
  */
